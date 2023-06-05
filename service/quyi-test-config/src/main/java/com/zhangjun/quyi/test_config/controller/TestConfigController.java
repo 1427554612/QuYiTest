b4cfcb52bd850249591e1333214c257468107dcb
zhangjun <1427554612@qq.com>
2023-06-05 14:43:03 +0800

    test-config\api-autotest服务配置redis缓存

--- service/quyi-test-config/src/main/java/com/zhangjun/quyi/test_config/controller/TestConfigController.java
+++ service/quyi-test-config/src/main/java/com/zhangjun/quyi/test_config/controller/TestConfigController.java
@@ -69,6 +69,7 @@ public class TestConfigController {
     @ApiOperation(value = "添加测试配置")
     public ResultModel saveTestConfig(@ApiParam(name = "testConfig",value = "配置对象")
                                           @Validated @RequestBody TestConfig testConfig){
+        System.out.println("呵呵呵");
         return ResultModel.ok().data("testConfig",testConfigService.saveConfig(testConfig));
     }
 
