6a1c2c55524bdc72cf761de29c7c044c69aedf98
zhangjun <1427554612@qq.com>
2023-08-11 21:34:17 +0800

    增加script测试模块

--- service/quyi-script-java/src/main/java/com/zhangjun/quyi/script_java/script_sty/jackson_demo/JsonScript.java
+++ service/quyi-script-java/src/main/java/com/zhangjun/quyi/script_java/script_sty/jackson_demo/JsonScript.java
@@ -16,6 +16,6 @@ public class JsonScript {
         String json = "{\"code\":200,\"data\":{\"list\":[],\"total\":0,\"page\":1,\"pageSize\":100},\"msg\":\"OK\"}";
         HashMap<String,Object> hashMap = JsonUtil.objectMapper.readValue(json, HashMap.class);
         System.out.println(JsonUtil.objectMapper.writeValueAsString(hashMap));
-
+        System.out.println("增加一行测试代码");
     }
 }
